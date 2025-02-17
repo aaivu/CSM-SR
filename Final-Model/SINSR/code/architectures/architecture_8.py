0d211fb6f3f7939afabac8c4c06f52da1f0c6031
Randika Prabashwara <139326591+randikapra@users.noreply.github.com>
2025-02-17 23:25:07 +0530

    Update architecture_8.py
    enhance the version rrdb from 4 to 5

--- Final-Model/SINSR/code/architectures/architecture_8.py
+++ Final-Model/SINSR/code/architectures/architecture_8.py
@@ -36,7 +36,7 @@ def residual_dense_block(x, filters, growth_rate=4, layers_in_block=4):
     return x
 
 # Define Residual-in-Residual Dense Block (RRDB)
-def rrdb(x, filters, growth_rate=4, res_block=4):
+def rrdb(x, filters, growth_rate=4, res_block=5):
     res = layers.Conv2D(filters, (3, 3), padding='same')(x)
     for _ in range(res_block):
         x = residual_dense_block(x, filters, growth_rate)
